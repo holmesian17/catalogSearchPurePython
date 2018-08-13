fa83b92468ecfafe25abe09b5a90c6cedf13e533
Ian <holmesian17@gmail.com>
2018-08-13 10:50:17 -0400

    Create catSearch.py

---
+++ catSearch.py
@@ -0,0 +1,16 @@
+records = []
+
+class record():
+    name = "book"
+    description = "this is a description. whatever." 
+    rankingAdditive = 0
+    numberOfHolds = 0
+
+def lookyLoo(record):
+    
+def doASearch():
+    searchTerm = input('Search for something: ')
+    if searchTerm in records:
+        for 
+
+def showSearchResults():
